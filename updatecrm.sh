6b432a0ec1ffdb9e447b077a3de4c5e731144f9f
Delore <delore23@gmail.com>
2023-11-30 22:57:02 -0300

    commit changes: Ajustes

--- updatecrm.sh
+++ updatecrm.sh
@@ -4,20 +4,14 @@ echo "update"
 echo "git pull"
 git pull
 
-echo "docker stop"
-docker stop delore-crm-srv_apizapalphaville_1 -t 0
-
 echo "docker build image"
 docker build -t delore23/api-zap-alphaville:latest .
-
-echo "docker rm"
-docker container rm delore-crm-srv_apizapalphaville_1
 
 echo "go to delore-crm-srv"
 cd
 cd delore-crm-srv/
 
 echo "restart docker compose"
-docker-compose up -d
+docker-compose restart
 
 echo "fim :)"
--- updateerp.sh
+++ updateerp.sh
@@ -4,21 +4,14 @@ echo "update"
 echo "git pull"
 git pull
 
-echo "docker stop"
-docker stop delore-erp-srv_apizapalphaville_1 -t 0
-
 echo "docker build image"
 docker build -t delore23/api-zap-alphaville:latest .
-
-echo "docker rm"
-docker container rm delore-erp-srv_apizapalphaville_1
 
 echo "go to delore-erp-srv"
 cd
 cd delore-erp-srv/
 
 echo "restart docker compose"
-docker-compose up -d
-
+docker-compose restart
 
 echo "fim :)"
